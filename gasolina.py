cf192bbb7d110628ddbb99c0ad40c0fb4500d09d
AnthonyCavalcante <anthonycavalcante@gmail.com>
2022-02-01 14:50:04 +0000

    Título do grafico alterado no arquivo gasolina.py

--- gasolina.py
+++ gasolina.py
@@ -8,6 +8,6 @@ gas_df = pd.read_csv('gasolina.csv')
 with sns.axes_style('darkgrid'):
   fig, ax = plt.subplots(figsize=(16,5))
   graffic = sns.lineplot(data=gas_df, x='dia' , y='venda' , palette='pastel')
-  graffic.set_title('Preço da Gasolina por dia', size=16, fontweight='bold')
-  graffic.set(xlabel='dia', ylabel='venda (R$)')
+  graffic.set_title('Preço diário de venda da Gasolina', size=24, fontweight='bold')
+  graffic.set(xlabel='dia', ylabel='Preço (R$)')
   plt.savefig('gasolina.png')
